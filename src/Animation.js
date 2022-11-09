a243a9eafe20747ce89b38870701abc0c67015cc
cyc99 <62579623+cyc99@users.noreply.github.com>
2022-11-10 00:30:33 +0900

    Add full page scroll animation

--- src/Animation.js
+++ src/Animation.js
@@ -1,67 +1,157 @@
 import './AnimationTest.css';
-import React from 'react';
+import React, { useRef } from 'react';
 import styled, {keyframes} from 'styled-components';
 import { useEffect, useState } from 'react'; 
 
 const AnimationTest = () => {
-  const [c,d] = useState(false);
-  const [t,g] = useState("NICE");
+  const ref = useRef()
+  // const [idx, setIdx] = useState(1)
 
-  useEffect(() => {
-    const k = setInterval(() => {
-      g(c ? "GOOD" : "NICE");
-      d(c ? false : true);
-    }, 5000);
-    return () => clearInterval(k);
-  });
+  useEffect (() => {
+    const curr = ref.current
+
+    const handler = (e) => {
+      e.preventDefault()
+      const innHeight = window.innerHeight
+      const {deltaY} = e
+      const {scrollTop} = curr
+      console.log([scrollTop, innHeight])
+      if (deltaY > 0) {
+        if (scrollTop < innHeight * 0.9) {
+          curr.scrollTo({
+            top: innHeight,
+            left: 0,
+            behavior: "smooth",
+          })
+        } else if (scrollTop < innHeight * 1.9) {
+          curr.scrollTo({
+            top: innHeight * 2,
+            left: 0,
+            behavior: "smooth",
+          })
+        } else {
+          curr.scrollTo({
+            top: innHeight * 3,
+            left: 0,
+            behavior: "smooth",
+          })
+        }
+      } else {
+        if (scrollTop >= innHeight * 2.9) {
+          curr.scrollTo({
+            top: innHeight * 2,
+            left: 0,
+            behavior: "smooth",
+          })
+        } else if (scrollTop >= innHeight * 1.9) {
+          curr.scrollTo({
+            top: innHeight,
+            left: 0,
+            behavior: "smooth",
+          })
+        } else {
+          curr.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth",
+          })
+        }
+      }
+    }
+
+    const resizeHandler = (e) => {
+      e.preventDefault()
+      const innHeight = window.innerHeight
+      const {scrollTop} = curr
+      if (scrollTop < innHeight * 0.5) {
+        curr.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: "smooth",
+        })
+      } else if (scrollTop < innHeight * 1.5 && scrollTop >= innHeight * 0.5) {
+        curr.scrollTo({
+          top: innHeight,
+          left: 0,
+          behavior: "smooth",
+        })
+      } else if (scrollTop < innHeight * 2.5 && scrollTop >= innHeight * 1.5) {
+        curr.scrollTo({
+          top: innHeight * 2,
+          left: 0,
+          behavior: "smooth",
+        })
+      } else {
+        curr.scrollTo({
+          top: innHeight * 3,
+          left: 0,
+          behavior: "smooth",
+        })
+      }
+    }
+
+    curr.addEventListener('wheel', handler)
+    window.addEventListener('resize', resizeHandler)
+
+    return () => {
+      curr.removeEventListener('wheel', handler)
+      window.removeEventListener('resize', resizeHandler)
+    }
+  })
 
 	return(
-    	<div className="grid_con">
-          <div className="item"><TESS>{t}</TESS>
-          </div>
-        </div>
-    )
+    <Outer ref={ref}>
+      <Inner>1</Inner>
+      <Inner2>2</Inner2>
+      <Inner3>3</Inner3>
+      <Inner4>4</Inner4>
+    </Outer>
+  )
 }
 
-const move = keyframes`
-0% {
-  opacity: 0;
-}
-10% {
-  opacity: 0;
-}
-25% {
-  opacity: 1;
-}
-75% {
-  opacity: 1;
-}
-90% {
-  opacity: 0;
-}
-100% {
-  opacity: 0;
-}
+const Inner = styled.div`
+height: 100vh;
+display: flex;
+justify-content: center;
+align-items: center;
+font-size: 100px;
+background: red;
 `;
 
-const Box = styled.div`
-    width: 300px;
-    height: 300px;
-    border-radius: 150px;
-    //여기서부터는 animation과 연관 있음
-    background: green;
-    position: absolute;
-    top: 20px;
-    left: 20px;
-    opacity: 1;
-    animation: ${move} 5s infinite;
+const Inner2 = styled.div`
+height: 100vh;
+display: flex;
+justify-content: center;
+align-items: center;
+font-size: 100px;
+background: blue;
+`;
+const Inner3 = styled.div`
+height: 100vh;
+display: flex;
+justify-content: center;
+align-items: center;
+font-size: 100px;
+background: green;
+`;
+const Inner4 = styled.div`
+height: 100vh;
+display: flex;
+justify-content: center;
+align-items: center;
+font-size: 100px;
+background: yellow;
 `;
 
-const TESS = styled.div`
-    font-size: 3rem;
-    position: absolute;
-    opacity: 1;
-    animation: ${move} 5s infinite;
+
+const Outer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  overflow-y: auto;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 export default AnimationTest;
